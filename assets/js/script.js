be2fb947db8dd90e4755759c0af3297cab02d5e8
Iosif <yaiosifua@gmail.com>
2020-12-13 15:12:03 +0200

    sidebar pointer update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,14 @@
 SCRIPT
 ==========================================================================*/
 
-const menuCenter = document.querySelector('.menu-center');
+const menuCenterList = document.querySelector('.menu-center__list');
+const listUl = menuCenterList.querySelector('ul');
 
 const menuItems = document.getElementsByClassName('menu-center__item');
 for(const item of menuItems) {
 	item.onclick = () => {
 		const element = item.querySelector('div');
-		menuCenter.classList.remove(menuCenter.classList[1]);
-		menuCenter.classList.add(`${element.classList[0]}-active`);
+		if (listUl.classList[0]) { listUl.classList.remove(listUl.classList[0]); }
+		listUl.classList.add(`${element.classList[0]}-active`);
 	};
 }
