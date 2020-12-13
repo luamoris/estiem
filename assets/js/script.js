5c6879470db824ff3e282a3a87f2cd6371ae2055
Iosif <yaiosifua@gmail.com>
2020-12-13 20:43:46 +0200

    header and footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,25 @@ for(const item of menuItems) {
 		listUl.classList.add(`${element.classList[0]}-active`);
 	};
 }
+
+// Buttons 
+
+const search = document.querySelector('.search');
+const searchInput = search.querySelector('.search__input');
+const searchCancel = search.querySelector('.search__cancel');
+
+searchInput.oninput = () => {
+	if (searchInput.value.length > 0) {
+		searchCancel.classList.remove('none');
+		searchCancel.classList.add('active');
+	} else {
+		searchCancel.classList.remove('active');
+		searchCancel.classList.add('none');
+	}
+}
+
+searchCancel.onclick = () => {
+	searchInput.value = '';
+	searchCancel.classList.remove('active');
+	searchCancel.classList.add('none');
+}
