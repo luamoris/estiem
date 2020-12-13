50f952ab5e639dde1630a95ad3b4bfce80ab1bb6
Iosif <yaiosifua@gmail.com>
2020-12-13 14:25:35 +0200

    sidebar pointer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,13 @@
 SCRIPT
 ==========================================================================*/
 
-const main = 100;
+const menuCenter = document.querySelector('.menu-center');
+
+const menuItems = document.getElementsByClassName('menu-center__item');
+for(const item of menuItems) {
+	item.onclick = () => {
+		const element = item.querySelector('div');
+		menuCenter.classList.remove(menuCenter.classList[1]);
+		menuCenter.classList.add(`${element.classList[0]}-active`);
+	};
+}
